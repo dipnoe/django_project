ab9933561683408862c3ed1ed3da0830cc6b95d1
Vagner Elena <dipnoe@gmail.com>
2023-08-03 01:42:13 +0300

    create views

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 
+from catalog.models import Category, Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Category.objects.all()[:3],
+        'title': "It`s all in the DNA"
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def category(request):
+    context = {
+        'object_list': Category.objects.all()
+    }
+    return render(request, 'catalog/category.html', context)
+
+
+def product(request, pk):
+    context = {
+        'object_list': Product.objects.filter(category=pk)
+    }
+    return render(request, 'catalog/product.html', context)
 
 
 def contacts(request):
