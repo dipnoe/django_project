a6ec2d4b7293980de44bd18a73fc529498376870
Vagner Elena <dipnoe@gmail.com>
2023-08-06 21:53:43 +0300

    add new registration

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Blog
 
 
 @admin.register(Category)
@@ -13,3 +13,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'create_date', 'is_published',)
