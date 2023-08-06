9865e3824ad967034e97de470a06cc2e98cbe40b
Vagner Elena <dipnoe@gmail.com>
2023-08-06 21:55:34 +0300

    CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DetailView, DeleteView
+from pytils.translit import slugify
 
 from catalog.models import Category, Product, Blog
 
@@ -24,6 +25,11 @@ class CategoryView(ListView):
 class ProductView(ListView):
     model = Product
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(category_id=self.kwargs.get('pk'))
+        return queryset
+
 
 def contacts(request):
     if request.method == 'POST':
@@ -36,4 +42,45 @@ def contacts(request):
 
 class BlogView(ListView):
     model = Blog
-    fields = ['title', 'body', '']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ['title', 'body', 'is_published']
+    success_url = reverse_lazy('blog')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+
+        return super().form_valid(form)
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'body', 'is_published']
+
+    def get_success_url(self):
+        return reverse('detail_blog', args=[self.object.pk])
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog')
