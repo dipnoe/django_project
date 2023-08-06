a81e755b431dbcd17cc78012aafbaec4718bdb17
Vagner Elena <dipnoe@gmail.com>
2023-08-06 15:52:17 +0300

    new model Blog

--- catalog/models.py
+++ catalog/models.py
@@ -31,3 +31,21 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.CharField(max_length=250, verbose_name='slug')
+    body = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='previews/blog/', verbose_name='Превью', **NULLABLE)
+    create_date = models.DateTimeField(verbose_name='Дата создания', auto_now=True)
+    is_published = models.BooleanField(default=False, verbose_name='опубликовано')
+    views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
+        ordering = ['-create_date']
